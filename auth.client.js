b2d88aab600d785d28bb08275895ed2341a1ad7b
Alberto Pose <albertopose@gmail.com>
2014-01-07 11:01:25 -0300

    Adding code sample.

---
+++ auth.client.js
@@ -0,0 +1,56 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('UserCtrl', function ($scope, $http, $window) {
+  $scope.user = {username: 'john.doe', password: 'foobar'};
+  $scope.message = '';
+  $scope.submit = function () {
+    $http
+      .post('/authenticate', $scope.user)
+      .success(function (data, status, headers, config) {
+        $window.sessionStorage.token = data.token;
+        $scope.message = 'Welcome';
+      })
+      .error(function (data, status, headers, config) {
+        // Erase the token if the user fails to log in
+        delete $window.sessionStorage.token;
+
+        // Handle login errors here
+        $scope.message = 'Error: Invalid user or password';
+      });
+  };
+});
+
+myApp.controller('RestrictedCtrl', function ($scope, $http) {
+  $scope.message = '';
+  $scope.callRestricted = function () {
+    $http({url: '/api/restricted', method: 'GET'})
+    .success(function (data, status, headers, config) {
+      $scope.message = data.name; // Should log 'foo'
+    })
+    .error(function (data, status, headers, config) {
+      $scope.message = data;
+    });
+  };
+});
+
+myApp.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.sessionStorage.token) {
+        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+        // handle the case where the user is not authenticated
+      }
+      return response || $q.when(response);
+    }
+  };
+});
+
+myApp.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
+});
---
+++ auth.server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var jwt = require('jsonwebtoken');  //https://npmjs.org/package/node-jsonwebtoken
+var expressJwt = require('express-jwt'); //https://npmjs.org/package/express-jwt
+
+var secret = 'this is the secret secret secret 12356';
+
+var app = express();
+
+// We are going to protect /api routes with JWT
+app.use('/api', expressJwt({secret: secret}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/', express.static(__dirname + '/'));
+
+app.use(function(err, req, res, next){
+  if (err.constructor.name === 'UnauthorizedError') {
+    res.send(401, 'Unauthorized');
+  }
+});
+
+app.post('/authenticate', function (req, res) {
+  //TODO validate req.body.username and req.body.password
+  //if is invalid, return 401
+  if (!(req.body.username === 'john.doe' && req.body.password === 'foobar')) {
+    res.send(401, 'Wrong user or password');
+    return;
+  }
+
+  var profile = {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'john@doe.com',
+    id: 123
+  };
+
+  // We are sending the profile inside the token
+  var token = jwt.sign(profile, secret, { expiresInMinutes: 60*5 });
+
+  res.json({ token: token });
+});
+
+app.get('/api/restricted', function (req, res) {
+  console.log('user ' + req.user.email + ' is calling /api/restricted');
+  res.json({
+    name: 'foo'
+  });
+});
+
+app.listen(8080, function () {
+  console.log('listening on http://localhost:8080');
+});
